7e2e43af34bc673fa076a7dcba93d577d5add113
Chief <freakoutbond2@gmail.com>
2023-01-21 17:42:31 +0300

    finished setup, dummy data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import './App.css';
+import SearchIcon from './searchIcon.svg';
 
 //omdbi api url
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=1e723a75"
@@ -13,13 +14,43 @@ function App() {
     console.log(data.Search);
   };
 
-  useEffect(()=> {
+  //dummy data
+  const movie1 = 
+    {
+      "Title": "Italian Spiderman",
+      "Year": "2007",
+      "imdbID": "tt2705436",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+  };
+
+  useEffect(() => {
     searchMovies('Spiderman');
   }, []);
 
   return (
     <div className="app">
-     <h1>MovieLando</h1>
+      <h1>MovieLando</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value="superman"
+          onChange={() => { }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search icon"
+          onClick={() => { }}
+        />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
