ca662e8c0000cdb74787e51ed1878ff4f48abe71
Chief <freakoutbond2@gmail.com>
2023-01-21 18:13:16 +0300

    fetch movies, handle no movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import SearchIcon from './searchIcon.svg';
 import MovieCard from "./MovieCard";
@@ -8,17 +8,31 @@ const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=1e723a75"
 
 function App() {
 
+  //state of movies
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [loading, setLoading] = useState(true);
+
   //function to search movies
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   //dummy data
   const movie1 =
   {
     "Title": "Italian Spiderman",
+    "Year": "2007",
+    "imdbID": "tt2705436",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+  };
+
+  const movie2 =
+  {
+    "Title": " Spiderman",
     "Year": "2007",
     "imdbID": "tt2705436",
     "Type": "movie",
@@ -45,9 +59,22 @@ function App() {
         />
       </div>
 
-      <div className="container">
-       <MovieCard movie1={movie1} />
-      </div>
+      {
+        movies?.length > 0
+          ? (
+            <div className="container">
+              <MovieCard movie1={movie1} />
+
+            </div>
+          ) :
+          (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )
+
+      }
+
     </div>
   );
 }
